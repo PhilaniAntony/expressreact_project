b2eb0fae9af50af117bc97bfbe0f4353e75c04e0
Philani_Antony <philaniantonydlamini@gmail.com>
2020-06-22 11:47:49 +0200

    Third Commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 //Initialise  express app
@@ -14,3 +15,13 @@ app.use("/api/user", require("./routes/user"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.listen(PORT);
+
+//Serve Static assest in production
+if (process.env.NODE_ENV === "production") {
+  //Set Static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
